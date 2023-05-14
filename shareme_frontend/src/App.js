eb448628c94d6f8ba1f8eccbdf87923e78a512bd
ZeenatFirdosh <zeenatzfq052@gmail.com>
2023-05-15 00:55:20 +0530

    shareme login

---
+++ shareme_frontend/src/App.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import './App.css';
+import Login from './components/Login';  //ctrl+space
+import Home from './container/Home';
+
+function App() {
+
+
+  
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>    
+  );
+}
+
+export default App;
